d5957eaad39133e3b78774f96fc5e00abbd53186
Rodney Dennis <rodneydennisjr@gmail.com>
2016-06-21 23:07:17 -0500

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,59 @@
+const btn = document.body.querySelector('#btn');
+const door = document.body.querySelector('#door');
+let status = 'closed';
+
+let h = door.offsetHeight/window.innerHeight * 100;
+
+let openId;
+let closeId;
+
+btn.addEventListener('click', () => {
+  switch(status) {
+    case 'closed':
+    case 'stopped while closing':
+      status = 'opening';
+      openId = setInterval(open, 500);
+      break;
+    case 'open':
+    case 'stopped while opening':
+      status = 'closing';
+      closeId = setInterval(close, 500);
+      break;
+    case 'closing':
+      status = 'stopped while closing'
+      stopDoor();
+      break;
+    default:
+      status = 'stopped while opening'
+      stopDoor();
+  }
+
+  console.log(status);
+});
+
+function open() {
+  h -= 10;
+  console.log(h);
+  door.style.height = `${h}%`;
+
+  if (door.style.height === '0%') {
+    console.log('cycle complete');
+    clearInterval(openId);
+  }
+}
+
+function close() {
+  h += 10;
+  console.log(h);
+  door.style.height = `${h}%`;
+
+  if (door.style.height === '100%') {
+    console.log('cycle complete');
+    clearInterval(closeId);
+  }
+}
+
+function stopDoor() {
+  clearInterval(openId);
+  clearInterval(closeId);
+}
